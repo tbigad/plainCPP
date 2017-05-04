e7447806aa934f5a97c6eca59beaaff7f95df1d2
Pavel N <tbigad@gmail.com>
2017-05-04 13:21:16 +0300

    update

--- training/parse_matrix_speed/main.cpp
+++ training/parse_matrix_speed/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const int SIZE = 1000;
-
+    const int SIZE = 1024;
     int arr[SIZE][SIZE];
+    srand(time(NULL));
 
     for(int i = 0; i < SIZE; i++)
     {
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 
     unsigned long v = 0;
 
-    unsigned int start_time =  clock(); // начальное время
+    unsigned int start_time =  clock();
 
     for(int i = 0; i < SIZE; i++)
     {
@@ -29,15 +30,15 @@ int main(int argc, char *argv[])
             v+= arr[i][j];
         }
     }
-    unsigned int end_time = clock(); // конечное время
-    unsigned int search_time = end_time - start_time; // искомое время
+    unsigned int end_time = clock();
+    unsigned int search_time = end_time - start_time;
 
     std::cout<< v << endl;
     std::cout<< "Time:" << search_time << endl;
 
     v = 0;
 
-    start_time =  clock(); // начальное время
+    start_time =  clock();
     for(size_t i = 0; i < SIZE; i++)
     {
         for(size_t j = 0; j < SIZE; j++)
@@ -45,8 +46,8 @@ int main(int argc, char *argv[])
             v+= arr[j][i];
         }
     }
-    end_time = clock(); // конечное время
-    search_time = end_time - start_time; // искомое время
+    end_time = clock();
+    search_time = end_time - start_time;
 
     std::cout<< v << endl;
     std::cout<< "Time:" << search_time << endl;
